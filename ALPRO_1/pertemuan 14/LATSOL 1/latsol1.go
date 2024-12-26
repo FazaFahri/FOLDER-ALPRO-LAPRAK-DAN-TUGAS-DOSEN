b071315789a02569854aabbd0828ab4bd3be45a8
MUHAMAD FAZA FAHRI AZIZ <fazafaza27f@gmail.com>
2024-12-26 15:12:46 +0700

    Add files via upload

---
+++ ALPRO_1/pertemuan 14/LATSOL 1/latsol1.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+    var n int
+    fmt.Scanln(&n)
+    count := 0
+    for i := 1; i <= n; i++ {
+        if i%2 != 0 { 
+            count++ 
+        }
+    }
+
+    fmt.Printf("Terdapat %d bilangan ganjil\n", count)
+}
